dae3a466ba61f04769013b0e04285ae2a4f2195a
Jason Cramer <jason.t.cramer@gmail.com>
2020-02-03 19:49:59 -0500

    Add script functionality to background extraction, and allow for class specific thresholds

--- cdsd/extract_background.py
+++ cdsd/extract_background.py
@@ -3,30 +3,64 @@ import numpy as np
 import h5py
 from cdsd.decrypt import read_encrypted_tar_audio_file
 import io
-import itertools
 import soundfile as sf
-import operator
+import sys
+import argparse
+from collections import defaultdict
+from functools import partial
+import multiprocessing
+import json
+import tqdm
 
 
-def mask_to_segment_idxs(mask, sample_rate=48000, hop_size=0.5):
-    hop_length = int(sample_rate * hop_size)
-    segment_idxs_list = []
-    for key, it in itertools.groupby(enumerate(mask), key=operator.itemgetter(1)):
-        # Skip masked values
-        if not key:
-            continue
+SAMPLE_RATE = 48000
 
-        # Compute start and end sample idxs from frame idxs
-        all_frame_idxs = np.array(list(zip(*it))[0])
-        start_idx = hop_length * all_frame_idxs[0]
-        end_idx = hop_length * (all_frame_idxs[-1] + 1)
-        segment_idxs_list.append((start_idx, end_idx))
 
-    return segment_idxs_list
+def process_arguments(args):
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('index_path', type=str,
+                        help='Path to index path or indices folder')
+
+    parser.add_argument('data_folder', type=str,
+                        help='Path to root sonyc data folder')
+
+    parser.add_argument('output_folder', type=str,
+                        help='Path to folder to output openl3 collections')
+
+    parser.add_argument('--threshold-path', type=str,
+                        help='Path to class-wise threshold JSON file')
+
+    parser.add_argument('--default-threshold', type=float,
+                        default=0.1,
+                        help='Default threshold value for all classes. Ignored if --threshold-path is specified.')
+
+    parser.add_argument('--cls-version', type=str,
+                        default='1.0.0',
+                        help='Classifier version to use for class predictions.')
+
+    parser.add_argument('--decrypt-url', type=str,
+                        default="https://decrypt-sonyc.engineering.nyu.edu/decrypt",
+                        help='SONYC decryption server URL')
+
+    parser.add_argument('--cacert-path', type=str,
+                        help='SONYC decryption CA certification path')
+
+    parser.add_argument('--cert-path', type=str,
+                        help='SONYC decryption certification path')
+
+    parser.add_argument('--key-path', type=str,
+                        help='SONYC decryption key path')
+
+    parser.add_argument('--num-cpus', type=int, default=1,
+                        help='Number of cpus to use.')
+
+    return parser.parse_args(args)
 
 
 def extract_background_audio(index_path, data_dir, output_dir,
-                             cls_version="1.0.0", threshold=0.1, sample_rate=48000,
+                             cls_version="1.0.0", threshold_path=None,
+                             default_threshold=0.1,
                              decrypt_url=None, cacert_path=None,
                              cert_path=None, key_path=None):
     year_str = os.path.split(os.path.dirname(index_path))[-1]
@@ -45,6 +79,12 @@ def extract_background_audio(index_path, data_dir, output_dir,
         print("No available audio. Skipping.")
         return
 
+    if threshold_path is None:
+        threshold_dict = defaultdict(lambda: default_threshold)
+    else:
+        with open(threshold_path, 'r') as f:
+            threshold_dict = json.load(f)
+
     print("Reading class predictions file.")
     cls_pred_path = os.path.join(data_dir, 'class_predictions',
                                  cls_version, year_str)
@@ -57,36 +97,39 @@ def extract_background_audio(index_path, data_dir, output_dir,
         if cls_pred_len != index_len:
             print("Index length differs from class prediction file length. Skipping.")
             return
+        labels = [key for key in cls_pred_h5['coarse'][0].dtype.names
+                  if 'timestamp' not in key or 'filename' not in key]
+        foreground_mask_list = np.zeros(len(labels)).astype(bool)
 
-        # Get the maximum class prediction for each element
-        max_cls_pred_list = None
-        for key in cls_pred_h5['coarse'][0].dtype.names:
-            if 'timestamp' not in key or 'filename' not in key:
-                if max_cls_pred_list is None:
-                    max_cls_pred_list = cls_pred_h5['coarse'][key]
-                else:
-                    max_cls_pred_list = np.maximum(max_cls_pred_list,
-                                          cls_pred_h5['coarse'][key])
+        # Compute classwise presence using class-specific thresholds and take logical-OR
+        for key in labels:
+            threshold = threshold_dict[key]
+            assert 0 < threshold < 1
+            cls_mask_list = cls_pred_h5['coarse'][key] >= threshold
+            foreground_mask_list = np.logical_or(foreground_mask_list, cls_mask_list)
+
+        # Negate mask to get background
+        background_mask_list = np.logical_not(foreground_mask_list)
 
     print("Grouping index elements by audio filename.")
     # First do an initial pass to group
     day_index_dict = {}
     timestamp_list_dict = {}
-    cls_pred_list_dict = {}
-    for timestamp, day_hdf5_path, day_hdf5_index, max_cls_pred in zip(timestamp_list, day_hdf5_path_list, day_hdf5_index_list, max_cls_pred_list):
+    background_mask_list_dict = {}
+    for timestamp, day_hdf5_path, day_hdf5_index, background_mask in zip(timestamp_list, day_hdf5_path_list, day_hdf5_index_list, background_mask_list):
         if day_hdf5_path not in day_index_dict:
             day_index_dict[day_hdf5_path] = []
             timestamp_list_dict[day_hdf5_path] = []
-            cls_pred_list_dict[day_hdf5_path] = []
+            background_mask_list_dict[day_hdf5_path] = []
         day_index_dict[day_hdf5_path].append(day_hdf5_index)
         timestamp_list_dict[day_hdf5_path].append(timestamp)
-        cls_pred_list_dict[day_hdf5_path].append(max_cls_pred)
+        background_mask_list_dict[day_hdf5_path].append(background_mask)
 
     num_files = len(day_index_dict)
     for file_idx, day_hdf5_path in enumerate(day_index_dict.keys()):
         print("* Processing audio for {} ({}/{})".format(day_hdf5_path, file_idx+1, num_files))
         day_index_list = day_index_dict[day_hdf5_path]
-        cls_pred_list = cls_pred_list_dict[day_hdf5_path]
+        background_mask_list = background_mask_list_dict[day_hdf5_path]
 
         day_hdf5_path = day_hdf5_path.decode()
         audio_hdf5_path = os.path.join(data_dir, day_hdf5_path)
@@ -95,8 +138,8 @@ def extract_background_audio(index_path, data_dir, output_dir,
             print("No audio clips for {}, skipping...".format(day_hdf5_path))
 
         with h5py.File(audio_hdf5_path, 'r') as audio_h5:
-            for day_hdf5_index, cls_pred in zip(day_index_list, cls_pred_list):
-                if cls_pred >= threshold:
+            for day_hdf5_index, background_mask in zip(day_index_list, background_mask_list):
+                if not background_mask:
                     continue
 
                 # Read audio
@@ -105,7 +148,7 @@ def extract_background_audio(index_path, data_dir, output_dir,
                 tar_data = io.BytesIO(audio_h5['recordings'][idx]['data'])
                 audio = read_encrypted_tar_audio_file(filename.decode(),
                                                       enc_tar_filebuf=tar_data,
-                                                      sample_rate=sample_rate,
+                                                      sample_rate=SAMPLE_RATE,
                                                       url=decrypt_url,
                                                       cacert=cacert_path,
                                                       cert=cert_path,
@@ -118,5 +161,39 @@ def extract_background_audio(index_path, data_dir, output_dir,
                 audio_path = os.path.join(audio_output_dir, fname)
 
                 # Write audio clip
-                sf.write(audio_path, audio, samplerate=sample_rate)
+                sf.write(audio_path, audio, samplerate=SAMPLE_RATE)
                 print("Saved {}".format(audio_path))
+
+
+if __name__ == '__main__':
+    params = process_arguments(sys.argv[1:])
+
+    # Set up TF session
+    index_path = os.path.abspath(params.index_path)
+    worker_func = partial(extract_background_audio,
+                          data_dir=os.path.abspath(params.data_folder),
+                          output_dir=os.path.abspath(params.output_folder),
+                          threshold_path=os.path.abspath(params.threshold_path),
+                          default_threshold=params.default_threshold,
+                          cls_version=params.cls_version,
+                          decrypt_url=params.decrypt_url,
+                          cacert_path=params.cacert_path,
+                          cert_path=params.cert_path,
+                          key_path=params.key_path)
+
+    if os.path.isdir(index_path):
+        index_path_list = [os.path.join(root, fname)
+                           for root, _, files in os.walk(index_path)
+                           for fname in files]
+    else:
+        assert os.path.isfile(index_path)
+        index_path_list = [index_path]
+
+    if params.num_cpus > 1 and len(index_path_list) > 1:
+        with multiprocessing.Pool(processes=params.num_cpus) as pool:
+            for _ in tqdm.tqdm(pool.imap_unordered(worker_func, index_path_list),
+                               total=len(index_path_list)):
+                pass
+    else:
+        for index_path in tqdm.tqdm(index_path_list):
+            worker_func(index_path)
