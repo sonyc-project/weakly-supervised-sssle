303a23a0b672b000d8763febea293b98b5b614cc
Jason Cramer <jason.t.cramer@gmail.com>
2020-02-25 22:08:42 -0500

    Add script for evaluating classifier

---
+++ cdsd/evaluate_classifier.py
@@ -0,0 +1,91 @@
+import argparse
+import json
+import os
+import sys
+import pandas as pd
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from data import get_data_transforms, CDSDDataset
+from models import construct_separator, construct_classifier
+
+
+def parse_arguments(args):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('root_data_dir',
+                        type=str,
+                        help='Path to dataset directory')
+
+    parser.add_argument('train_config_path',
+                        type=str,
+                        help='Path to training configuration JSON file')
+
+    parser.add_argument('--output-dir',
+                        type=str,
+                        help='Path where outputs will be saved. Defaults to the one specified in the train configuration file.')
+
+    parser.add_argument('-n', '--num-data-workers',
+                        type=int, default=1,
+                        help='Number of workers used for data loading.')
+
+    return parser.parse_args(args)
+
+
+def evaluate(root_data_dir, train_config, output_dir=None, num_data_workers=1):
+    # Create output directory
+    output_dir = output_dir or train_config["output_dir"]
+    os.makedirs(output_dir, exist_ok=True)
+    input_transform = get_data_transforms(train_config)
+
+    train_dataset = CDSDDataset(root_data_dir,
+                                subset='train',
+                                transform=input_transform)
+    classifier = construct_classifier(train_config,
+                                      dataset=train_dataset,
+                                      require_init=True)
+
+    for subset in ('train', 'valid', 'test'):
+        print('====== Evaluating subset "{}" ======'.format(subset))
+        dataset = CDSDDataset(root_data_dir,
+                              subset=subset,
+                              transform=input_transform)
+        dataloader = DataLoader(dataset, batch_size=train_config["training"]["batch_size"],
+                                shuffle=False, pin_memory=True,
+                                num_workers=num_data_workers)
+        num_batches = len(dataloader)
+
+        # Initialize results lists
+        subset_results = {label + "_presence_gt": [] for label in dataset.labels}
+        subset_results.update({label + "_presence_pred": [] for label in dataset.labels})
+        subset_results["filenames"] = list(dataset.files) # Assuming that dataloader preserves order
+
+        subset_results_path = os.path.join(output_dir, "classification_results_{}.csv".format(subset))
+
+        for batch in tqdm(dataloader, total=num_batches):
+            x = batch["audio_data"]
+            labels = batch["labels"]
+            # Run classifier on mixture for later analysis
+            pred = classifier(x)
+
+            for idx, label in enumerate(train_dataset.labels):
+                subset_results[label + "_presence_gt"] += labels[:, idx].tolist()
+                subset_results[label + "_presence_pred"] += pred[:, idx].tolist()
+
+        # Save results as CSV
+        subset_df = pd.DataFrame(subset_results)
+        subset_df.to_csv(subset_results_path)
+
+        print("Saved results to {}".format(subset_results_path))
+
+
+if __name__ == "__main__":
+    args = parse_arguments(sys.argv[1:])
+
+    # Load training config
+    with open(args.train_config_path, 'r') as f:
+        train_config = json.load(f)
+
+    evaluate(root_data_dir=args.root_data_dir,
+             train_config=train_config,
+             output_dir=args.output_dir,
+             num_data_workers=args.num_data_workers)
