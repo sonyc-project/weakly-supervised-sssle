Fix class level prior weighting

cdsd/data.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import warnings
 import jams
+import librosa
 import torch
 import torchaudio
 import torchvision
@@ -72,47 +73,29 @@ class CDSDDataset(Dataset):
 
         total_frames = 0
         frame_label_counts = torch.zeros(self.num_labels)
-        for ex in self:
-            frame_labels = ex['frame_labels']
+        for idx in range(len(self.files)):
+            frame_labels = self.get_labels(idx)['frame_labels']
             total_frames += frame_labels.size()[0]
             frame_label_counts += frame_labels.sum(dim=0)
         total_frames = torch.tensor(total_frames, dtype=torch.float32)
-
         self.class_frame_priors = frame_label_counts / total_frames
 
     def __len__(self):
         return len(self.files)
 
-    def get_num_frames(self):
-        file = self.files[0]
-        audio_path = os.path.join(self.data_dir, file + '.wav')
-        audio_data, sr = torchaudio.load(audio_path)
-        if self.transform is not None:
-            audio_data = self.transform(audio_data)
-
-        return audio_data.size()[-1]
-
-    def __getitem__(self, idx):
+    def get_labels(self, idx):
         if torch.is_tensor(idx):
             idx = idx.item()
 
         file = self.files[idx]
         audio_path = os.path.join(self.data_dir, file + '.wav')
         jams_path = os.path.join(self.data_dir, file + '.jams')
-
-        waveform, sr = torchaudio.load(audio_path)
-
-        if sr != SAMPLE_RATE:
-            raise ValueError('Expected sample rate of {} Hz, but got {} Hz ({})'.format(SAMPLE_RATE, sr, audio_path))
-
-        waveform_len = waveform.size()[-1]
-        audio_data = waveform
+        waveform_len = int(librosa.get_duration(filename=audio_path) * SAMPLE_RATE)
 
         is_stft = False
         hop_length = 1
         pad_ts = 0.0
         if self.transform is not None:
-            audio_data = self.transform(audio_data)
             for t in self.transform.transforms:
                 # There should only be at most one transform that
                 # effects the time dimension (since we don't allow
@@ -124,7 +107,8 @@ class CDSDDataset(Dataset):
                     pad_ts = (t.win_length // 2) / SAMPLE_RATE
                     break
         hop_ts = hop_length / SAMPLE_RATE
-        num_frames = audio_data.size()[-1]
+        # Assumes centering
+        num_frames = waveform_len // hop_length + 1
 
         jams_obj = jams.load(jams_path)
 
@@ -158,10 +142,51 @@ class CDSDDataset(Dataset):
                 # Increase event count
                 num_events += 1
 
-        sample = {
-            'audio_data': audio_data,
+        return {
             'clip_labels': clip_label_arr,
             'frame_labels': frame_label_arr,
+            'num_events': num_events,
+            'is_stft': is_stft
+        }
+
+    def get_num_frames(self):
+        file = self.files[0]
+        audio_path = os.path.join(self.data_dir, file + '.wav')
+        waveform_len = int(librosa.get_duration(filename=audio_path) * SAMPLE_RATE)
+
+        hop_length = 1
+        if self.transform is not None:
+            for t in self.transform.transforms:
+                if isinstance(t, Spectrogram) or isinstance(t, MelSpectrogram):
+                    hop_length = t.hop_length
+                    break
+        # Assumes centering
+        num_frames = waveform_len // hop_length + 1
+
+        return num_frames
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.item()
+
+        file = self.files[idx]
+        audio_path = os.path.join(self.data_dir, file + '.wav')
+
+        waveform, sr = torchaudio.load(audio_path)
+
+        if sr != SAMPLE_RATE:
+            raise ValueError('Expected sample rate of {} Hz, but got {} Hz ({})'.format(SAMPLE_RATE, sr, audio_path))
+
+        waveform_len = waveform.size()[-1]
+        audio_data = waveform
+
+        labels_dict = self.get_labels(idx)
+        is_stft = labels_dict['is_stft']
+
+        sample = {
+            'audio_data': audio_data,
+            'clip_labels': labels_dict['clip_labels'],
+            'frame_labels': labels_dict['frame_labels'],
             'index': torch.tensor(idx, dtype=torch.int16)
         }
 
@@ -173,7 +198,7 @@ class CDSDDataset(Dataset):
             sample['energy_mask'] = energy_mask.squeeze()
 
         if self.load_separation_data:
-            sample['num_events'] = num_events
+            sample['num_events'] = labels_dict['num_events']
 
             # Include mixture and separated source waveforms
             sample['mixture_waveform'] = waveform

cdsd/train_fully_supervised_separator.py
@@ -96,7 +96,6 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
     separator.to(device)
 
     class_prior_weighting = train_config["training"].get("class_prior_weighting", False)
-    class_frame_priors = train_dataset.class_frame_priors.to(device)
     energy_masking = train_config["losses"]["separation"].get("energy_masking", False)
 
     # JTC: Should we still provide params with requires_grad=False here?
@@ -138,6 +137,7 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
         torch.cuda.empty_cache()
         for batch_idx, batch in enumerate(tqdm(train_dataloader, total=num_train_batches)):
             x = batch["audio_data"].to(device)
+            frame_labels = batch["frame_labels"].to(device)
             energy_mask = batch["energy_mask"].to(device)
             curr_batch_size = x.size()[0]
             norm_factor = get_normalization_factor(x, energy_mask,
@@ -161,7 +161,14 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
 
             for label_idx, label in enumerate(train_dataset.labels):
                 if class_prior_weighting:
-                    weight = class_frame_priors[label_idx].to(device)
+                    cls_frame_labels = frame_labels[..., label_idx]
+                    p = train_dataset.class_frame_priors[label_idx]
+
+                    weight = torch.zeros_like(cls_frame_labels, dtype=x.dtype, device=device)
+                    weight[cls_frame_labels.bool()] = 1.0 / p
+                    weight[(-cls_frame_labels + 1).bool()] = 1.0 / (1 - p)
+                    weight = weight[:, None, None :]
+                    del cls_frame_labels
                 else:
                     weight = torch.ones(1, dtype=x.dtype, device=device)
 
@@ -196,7 +203,7 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
                 train_idxs_save = batch['index'][:num_debug_examples].cpu().numpy()
 
             # Cleanup
-            del x, masks, energy_mask, batch, mask, x_masked, train_loss, \
+            del x, masks, energy_mask, batch, mask, x_masked, train_loss, weight, \
                 norm_factor, src_spec, src_spec_diff, src_spec_diff_flat, src_loss
             torch.cuda.empty_cache()
 
@@ -205,6 +212,7 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
         with torch.no_grad():
             for batch_idx, batch in enumerate(tqdm(valid_dataloader, total=num_valid_batches)):
                 x = batch["audio_data"].to(device)
+                frame_labels = batch["frame_labels"].to(device)
                 energy_mask = batch["energy_mask"].to(device)
                 curr_batch_size = x.size()[0]
                 norm_factor = get_normalization_factor(x, energy_mask,
@@ -224,7 +232,14 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
 
                 for label_idx, label in enumerate(valid_dataset.labels):
                     if class_prior_weighting:
-                        weight = class_frame_priors[label_idx].to(device)
+                        cls_frame_labels = frame_labels[..., label_idx]
+                        p = train_dataset.class_frame_priors[label_idx]
+
+                        weight = torch.zeros_like(cls_frame_labels, dtype=x.dtype, device=device)
+                        weight[cls_frame_labels.bool()] = 1.0 / p
+                        weight[(-cls_frame_labels + 1).bool()] = 1.0 / (1 - p)
+                        weight = weight[:, None, None :]
+                        del cls_frame_labels
                     else:
                         weight = torch.ones(1, dtype=x.dtype, device=device)
 
@@ -255,7 +270,7 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
                     valid_idxs_save = batch['index'][:num_debug_examples].cpu().numpy()
 
                 # Cleanup
-                del x, masks, energy_mask, batch, mask, x_masked, valid_loss, \
+                del x, masks, energy_mask, batch, mask, x_masked, valid_loss, weight, \
                     norm_factor, src_spec, src_spec_diff, src_spec_diff_flat, src_loss
                 torch.cuda.empty_cache()
 

cdsd/train_separator.py
@@ -116,7 +116,6 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
         label_maxpool = None
 
     class_prior_weighting = train_config["training"].get("class_prior_weighting", False)
-    class_frame_priors = train_dataset.class_frame_priors.to(device)
 
     # JTC: Should we still provide params with requires_grad=False here?
     all_params = chain(separator.parameters(), classifier.parameters())
@@ -180,8 +179,19 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
                 # Downsample labels if necessary
                 if label_maxpool is not None:
                     cls_target_labels = label_maxpool(cls_target_labels.transpose(1, 2)).transpose(1, 2)
+
+                class_weights = torch.zeros_like(cls_target_labels, device=device)
+                frame_labels = None
+                for class_idx in range(train_dataset.num_labels):
+                    frame_labels = cls_target_labels[..., class_idx]
+                    p = train_dataset.class_frame_priors[class_idx]
+                    class_weights[frame_labels.bool(), class_idx] = 1.0 / p
+                    class_weights[(-frame_labels + 1).bool(), class_idx] = 1.0 / (1 - p)
+                del frame_labels
             else:
                 cls_target_labels = clip_labels
+                class_weights = None
+
             energy_mask = batch["energy_mask"].to(device)
 
             if epoch == 0 and batch_idx == 0:
@@ -204,7 +214,7 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
                 cls_bce = F.binary_cross_entropy(mix_cls_output,
                                                         cls_target_labels,
                                                         reduction='none')
-                cls_bce *= class_frame_priors[None, None, :]
+                cls_bce *= class_weights
                 train_cls_loss = cls_bce.mean()
             else:
                 cls_bce = None
@@ -226,7 +236,7 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
                     cls_bce = F.binary_cross_entropy(src_cls_output,
                                                      target,
                                                      reduction='none')
-                    cls_bce *= class_frame_priors[None, None, :]
+                    cls_bce *= class_weights
                     train_cls_loss += cls_bce.mean()
                 else:
                     cls_bce = None
@@ -253,7 +263,7 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
             # Cleanup
             del x, clip_labels, cls_bce, cls_target_labels, masks, energy_mask, batch,\
                 mask, x_masked, mix_cls_output, src_cls_output, train_cls_loss, \
-                train_mix_loss, train_total_loss
+                train_mix_loss, train_total_loss, class_weights
             torch.cuda.empty_cache()
 
         # Evaluate on validation set
@@ -267,8 +277,19 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
                     # Downsample labels if necessary
                     if label_maxpool is not None:
                         cls_target_labels = label_maxpool(cls_target_labels.transpose(1, 2)).transpose(1, 2)
+
+                    class_weights = torch.zeros_like(cls_target_labels, device=device)
+                    frame_labels = None
+                    for class_idx in range(train_dataset.num_labels):
+                        frame_labels = cls_target_labels[..., class_idx]
+                        p = train_dataset.class_frame_priors[class_idx]
+                        class_weights[frame_labels.bool(), class_idx] = 1.0 / p
+                        class_weights[(-frame_labels + 1).bool(), class_idx] = 1.0 / (1 - p)
+                    del frame_labels
                 else:
                     cls_target_labels = clip_labels
+                    class_weights = None
+
                 energy_mask = batch["energy_mask"].to(device)
 
                 # Set models to eval mode
@@ -289,7 +310,7 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
                     cls_bce = F.binary_cross_entropy(mix_cls_output,
                                                      cls_target_labels,
                                                      reduction='none')
-                    cls_bce *= class_frame_priors[None, None, :]
+                    cls_bce *= class_weights
                     valid_cls_loss = cls_bce.mean()
                 else:
                     cls_bce = None
@@ -311,7 +332,7 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
                         cls_bce = F.binary_cross_entropy(src_cls_output,
                                                          target,
                                                          reduction='none')
-                        cls_bce *= class_frame_priors[None, None, :]
+                        cls_bce *= class_weights
                         valid_cls_loss += cls_bce.mean()
                     else:
                         cls_bce = None
@@ -334,7 +355,7 @@ def train(root_data_dir, train_config, output_dir, num_data_workers=1,
                 # Help garbage collection
                 del x, clip_labels, cls_bce, cls_target_labels, energy_mask, masks,\
                     batch, mask, x_masked, mix_cls_output, src_cls_output, \
-                    valid_cls_loss, valid_mix_loss, valid_total_loss
+                    valid_cls_loss, valid_mix_loss, valid_total_loss, class_weights
                 torch.cuda.empty_cache()
 
         # Log losses